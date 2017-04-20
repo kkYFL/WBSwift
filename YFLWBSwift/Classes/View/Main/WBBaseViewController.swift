ea7d1c7128c6ee66e0b353e523315e67b6e22c17
杨丰林 <xiaoyang@maihaoche.com>
2017-04-20 17:44:48 +0800

    使用桥接文件引入OC代码

---
+++ YFLWBSwift/Classes/View/Main/WBBaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WBBaseViewController.swift
+//  YFLWBSwift
+//
+//  Created by 杨丰林 on 17/4/20.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+
+import UIKit
+
+class WBBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.cz_random()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+
+}
---
+++ YFLWBSwift/YFLWBSwift/WB-bridge.h
@@ -0,0 +1,10 @@
+//
+//  WB-bridge.h
+//  YFLWBSwift
+//
+//  Created by 杨丰林 on 17/4/20.
+//  Copyright © 2017年 杨丰林. All rights reserved.
+//
+//专门用来引入OC的文件，一旦引入就可以正常使用OC代码（宏除外，Swift中预编译指令都被取消了）
+
+#import "CZAdditions.h"
